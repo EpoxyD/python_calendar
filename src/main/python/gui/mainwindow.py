96c7bd35d8714782835410ad2c6aa52d3e36b751
Evert Borghgraef <evert.borghgraef@gmail.com>
2020-03-22 16:49:04 +0100

    Define rough definite gui

--- src/main/python/gui/mainwindow.py
+++ src/main/python/gui/mainwindow.py
@@ -2,10 +2,15 @@
 
 import sys
 
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QMessageBox
 from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtCore import pyqtSlot
 
-from src.main.python.gui.modules import CSVSelectModule, GenerateModule, TeamsModule
+from src.main.python.gui.csvmodule import CSVModule
+from src.main.python.gui.generatemodule import GenerateModule
+from src.main.python.gui.datamodule import DataModule
+
+from src.main.python.cal_controller import parse_csv
 
 
 class MainWindow(QWidget):
@@ -14,12 +19,15 @@ class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Calendar Generator")
-        self.setWindowIcon(QIcon(QPixmap("src/img/pool.ico")))
         self.setFixedSize(600, 300)
 
-        self.layout = QVBoxLayout()
-        self.setLayout(self.layout)
+        self.setLayout(QGridLayout())
+        self.layout().addWidget(CSVModule(self), 1, 1, 1, 2)
+        self.layout().addWidget(DataModule(self, "Competitions"), 2, 1, 3, 1)
+        self.layout().addWidget(DataModule(self, "Teams"), 2, 2, 3, 1)
+        self.layout().addWidget(GenerateModule(self), 5, 1, 1, 2)
 
-        self.layout.addWidget(CSVSelectModule(self), stretch=1)
-        self.layout.addWidget(TeamsModule(self), stretch=3)
-        self.layout.addWidget(GenerateModule(self), stretch=1)
+    @pyqtSlot()
+    def update_competitions(self):
+        print("update competitions")
+        parse_csv(self.findChild())
